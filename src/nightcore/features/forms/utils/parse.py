3fad531025b1f9eee5917155a6d082911db0c2d9
monok8i <didkivskiyyura725@gmail.com>
2026-03-29 13:17:45 +0200

    refactor(forms): enhance logging in parsing functions for better traceability

--- src/nightcore/features/forms/utils/parse.py
+++ src/nightcore/features/forms/utils/parse.py
@@ -1,15 +1,21 @@
 """Utils for parsing needed text from components."""
 
+import logging
 import re
 
 from discord import Component
+
+logger = logging.getLogger(__name__)
 
 
 def parse_form_text_from_components(components: list[Component]):  # type: ignore
     """Extracts the form text from a list of components."""
     for container in components:
+        logger.info("Childrens of container: %s", container.children)  # type: ignore
         for item in container.children:  # type: ignore
+            logger.info("Item: %s", item)  # type: ignore
             if item.id == 3:  # type: ignore
+                logger.info("Found item with id 3: %s", item)  # type: ignore
                 match = re.search(r"```([\s\S]*?)```", item.content)  # type: ignore
                 if match:
                     return match.group(1)
@@ -20,8 +26,11 @@ def parse_form_text_from_components(components: list[Component]):  # type: ignor
 def parse_author_id_from_components(components: list[Component]):  # type: ignore
     """Extracts the author ID from a list of components."""
     for component in components:
+        logger.info("Children of component: %s", component.children)  # type: ignore
         for item in component.children:  # type: ignore
+            logger.info("Item: %s", item)  # type: ignore
             if item.id == 1:  # type: ignore
+                logger.info("Found item with id 1: %s", item)  # type: ignore
                 match = re.search(r"<@!?(\d+)>", item.content)  # type: ignore
                 if match:
                     return int(match.group(1))
