4e43d1cf81cab48d966f53a7b56189c65a4652b7
monok8i <didkivskiyyura725@gmail.com>
2026-03-29 13:22:12 +0200

    fix(forms): correct item ID checks in form text and author ID parsing functions

--- src/nightcore/features/forms/utils/parse.py
+++ src/nightcore/features/forms/utils/parse.py
@@ -14,8 +14,7 @@ def parse_form_text_from_components(components: list[Component]):  # type: ignor
         logger.info("Childrens of container: %s", container.children)  # type: ignore
         for item in container.children:  # type: ignore
             logger.info("Item: %s", item)  # type: ignore
-            if item.id == 3:  # type: ignore
-                logger.info("Found item with id 3: %s", item)  # type: ignore
+            if item.id == 5:  # type: ignore
                 match = re.search(r"```([\s\S]*?)```", item.content)  # type: ignore
                 if match:
                     return match.group(1)
@@ -29,8 +28,7 @@ def parse_author_id_from_components(components: list[Component]):  # type: ignor
         logger.info("Children of component: %s", component.children)  # type: ignore
         for item in component.children:  # type: ignore
             logger.info("Item: %s", item)  # type: ignore
-            if item.id == 1:  # type: ignore
-                logger.info("Found item with id 1: %s", item)  # type: ignore
+            if item.id == 3:  # type: ignore
                 match = re.search(r"<@!?(\d+)>", item.content)  # type: ignore
                 if match:
                     return int(match.group(1))
